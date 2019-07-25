84154cab3dc9c0e9d1e30666b1ed9266c9f04822
serafima zykova <zykovas91@gmail.com>
2019-07-26 00:00:33 +0300

    LoginTextView

---
+++ Eatho/View/LoginTextView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTextView.swift
+//  Eatho
+//
+//  Created by Серафима Зыкова on 25/07/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextView: UITextView {
+
+    func setupView() {
+        
+    }
+
+}
