afb10cf80fa32c3674c6204fba73f4cbb90aed6b
serafima zykova <zykovas91@gmail.com>
2019-07-25 15:40:08 +0300

    designed login view

---
+++ Eatho/View/EathoButton.swift
@@ -0,0 +1,40 @@
+//
+//  EathoButton.swift
+//  Eatho
+//
+//  Created by Серафима Зыкова on 25/07/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class EathoButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+
+    func setupView() {
+//        let gradient = CAGradientLayer()
+//        
+//        gradient.colors = [#colorLiteral(red: 0.6862745098, green: 0.5411764706, blue: 0.9294117647, alpha: 1).cgColor, #colorLiteral(red: 0.5450980392, green: 0.5490196078, blue: 0.9294117647, alpha: 1).cgColor]
+//        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+//        gradient.endPoint = CGPoint(x: 0.6, y: 1)
+//        gradient.frame = self.bounds
+//        
+//        self.layer.insertSublayer(gradient, at: 0)
+        self.layer.cornerRadius = cornerRadius
+    }
+}
