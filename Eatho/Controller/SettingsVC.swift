f82546b9ec6b770ad27cb599de440bbf2a4821f2
serafima zykova <zykovas91@gmail.com>
2019-08-01 15:35:17 +0300

    changed settings view

--- Eatho/Controller/SettingsVC.swift
+++ Eatho/Controller/SettingsVC.swift
@@ -8,21 +8,25 @@
 
 import UIKit
 
-class SettingsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SettingsVC: UIViewController {
 
     //Outlets
-    @IBOutlet weak var kcalTxt: UITextField!
-    @IBOutlet weak var carbsTxt: UITextField!
-    @IBOutlet weak var proteinsTxt: UITextField!
-    @IBOutlet weak var fatsTxt: UITextField!
+    @IBOutlet weak var caloriesTxt: UITextField!
+    @IBOutlet weak var proteinsMassTxt: UITextField!
+    @IBOutlet weak var proteinsPercentTxt: UITextField!
+    @IBOutlet weak var fatsMassTxt: UITextField!
+    @IBOutlet weak var fatsPercentTxt: UITextField!
+    @IBOutlet weak var carbsMassTxt: UITextField!
+    @IBOutlet weak var carbsPercentTxt: UITextField!
     
-    @IBOutlet weak var autoParamsView: UIStackView!
     @IBOutlet weak var autoSwitch: UISwitch!
     @IBOutlet weak var genderSwitch: UISegmentedControl!
     @IBOutlet weak var weightTxt: UITextField!
     @IBOutlet weak var heightTxt: UITextField!
     @IBOutlet weak var ageTxt: UITextField!
-    @IBOutlet weak var activitySelection: UIPickerView!
+    @IBOutlet weak var activityTxt: UITextField!
+    @IBOutlet weak var caloriesShortageTxt: UITextField!
+    @IBOutlet weak var calculateBtn: EathoButton!
     
     var activityIndex: Int = 0
     
@@ -73,13 +77,15 @@ class SettingsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         dismiss(animated: true, completion: nil)
     }
 
-    @IBAction func autoKcalSettingChanged(_ sender: Any) {
+    @IBAction func autoSwitchChanged(_ sender: Any) {
         if autoSwitch.isOn {
             genderSwitch.isHidden = false
             weightTxt.isHidden = false
             heightTxt.isHidden = false
             ageTxt.isHidden = false
-            activitySelection.isHidden = false
+            activityTxt.isHidden = false
+            caloriesShortageTxt.isHidden = false
+            calculateBtn.isHidden = false
             
             UIView.animate(withDuration: 0.3, animations: {
                 self.view.layoutIfNeeded()
@@ -90,8 +96,39 @@ class SettingsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
                 self.weightTxt.isHidden = true
                 self.heightTxt.isHidden = true
                 self.ageTxt.isHidden = true
-                self.activitySelection.isHidden = true
+                self.activityTxt.isHidden = true
+                self.caloriesShortageTxt.isHidden = true
+                self.calculateBtn.isHidden = true
             })
         }
+    }
+
+    @IBAction func calculatePressed(_ sender: Any) {
+        guard let weightStr = weightTxt.text else { return }
+        guard let heightStr = heightTxt.text else { return }
+        guard let ageStr = ageTxt.text else { return }
+        let shortageStr = caloriesShortageTxt.text ?? "0"
+
+        let weight = Double(weightStr) ?? 0
+        let height = Double(heightStr) ?? 0
+        let age = Double(ageStr) ?? 0
+        let shortage = Double(shortageStr) ?? 0
+
+        var calories = 0.0, p = 0.0, c = 0.0, f = 0.0
+        SettingsService.instance.calculateNutrients(weightKg: weight, heightM: height / 100, age: age, gender: genderSwitch.selectedSegmentIndex, activityIndex: activityIndex, shortage: shortage, p: &p, c: &c, f: &f, calories: &calories)
+
+        caloriesTxt.text = "\(round(calories))"
+        proteinsMassTxt.text = "\(round(p))"
+        proteinsPercentTxt.text = "\(Int(round(p * 4.1 * 100 / calories)))"
+        carbsMassTxt.text = "\(round(c))"
+        carbsPercentTxt.text = "\(Int(round(c * 4.1 * 100 / calories)))"
+        fatsMassTxt.text = "\(round(f))"
+        fatsPercentTxt.text = "\(Int(round(f * 9.29 * 100 / calories)))"
+    }
+    
+    @IBAction func activityChange(_ sender: Any) {
+        let activityPicker = ActivityPickerVC()
+        activityPicker.modalPresentationStyle = .custom
+        present(activityPicker, animated: true, completion: nil)
     }
 }
--- Eatho/Services/SettingsService.swift
+++ Eatho/Services/SettingsService.swift
@@ -22,4 +22,34 @@ class SettingsService {
             defaults.set(newValue, forKey: IS_CONFIGURED)
         }
     }
+    
+    func leanMass(weightKg w: Double, heightM h: Double, age: Double, gender: Int) -> Double {
+        let bmi = w / (h * h)
+        let fatPercent = gender == 1 ? (1.51 * bmi - 0.7 * age - 3.6 + 1.4) : (1.51 * bmi - 0.7 * age + 1.4)
+        return (w * (100 - fatPercent) / 100)
+    }
+    
+    func calculateNutrients(weightKg w: Double, heightM h: Double, age: Double, gender: Int, activityIndex: Int, shortage: Double, p: inout Double, c: inout Double, f: inout Double, calories: inout Double) {
+
+        let lean = SettingsService.instance.leanMass(weightKg: w, heightM: h, age: age, gender: gender)
+        
+        switch activityIndex {
+        case 1:
+            p = 1.6 * lean
+        case 2:
+            p = 1.75 * lean
+        case 3:
+            p = 2 * lean
+        case 4:
+            p = 2.5 * lean
+        default:
+            p = 1.5 * lean
+        }
+        
+        let nShortage = shortage / 2
+        f = 1.1 * lean - nShortage / 9.29
+        c = 4 * lean - nShortage / 4.1
+        
+        calories = p * 4.1 + c * 4.1 + f * 9.29
+    }
 }
