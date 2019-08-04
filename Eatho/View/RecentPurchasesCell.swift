ce3db1177027d8f45797da7a509a73ba5b56952c
serafima zykova <zykovas91@gmail.com>
2019-08-04 09:12:34 +0300

    added shopping list view

---
+++ Eatho/View/RecentPurchasesCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecentPurchasesCell.swift
+//  Eatho
+//
+//  Created by Серафима Зыкова on 04/08/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import UIKit
+
+class RecentPurchasesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Eatho/View/ShoppingListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListCell.swift
+//  Eatho
+//
+//  Created by Серафима Зыкова on 04/08/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
