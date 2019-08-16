6a4cc3635874a64925ae2027ee58845bbec6e86b
serafima zykova <zykovas91@gmail.com>
2019-08-16 22:22:58 +0300

    added user data in table

--- Eatho/Controller/Available/DetailsVC.swift
+++ Eatho/Controller/Available/DetailsVC.swift
@@ -15,6 +15,7 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var chartView: NutritionChartView!
     @IBOutlet weak var fullInfoTableView: UITableView!
     
+    var userData = [Nutrient]()
     var macro = [Nutrient]()
     var minerals = [Nutrient]()
     var vitamins = [Nutrient]()
@@ -27,6 +28,7 @@ class DetailsVC: UIViewController {
         nameLbl.text = food.name
         icon.image = UIImage(named: food.icon)
         
+        userData = getUserData(food: food)
         macro = food.nutrition.getMacro(portion: food.delta ?? 0)
         minerals = food.nutrition.getMinerals(portion: food.delta ?? 0)
         vitamins = food.nutrition.getVitamins(portion: food.delta ?? 0)
@@ -37,21 +39,32 @@ class DetailsVC: UIViewController {
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    func getUserData(food: FoodItem) -> [Nutrient] {
+        var data = [Nutrient]()
+        data.append(Nutrient(name: "Available, g", perPorition: nil, per100g: food.availableWeight ?? 0, type: .main))
+        data.append(Nutrient(name: "Daily min portion, g", perPorition: nil, per100g: Double(food.dailyPortion.min ?? 0), type: .main))
+        data.append(Nutrient(name: "Daily max portion, g", perPorition: nil, per100g: Double(food.dailyPortion.max ?? 0), type: .main))
+        data.append(Nutrient(name: "Delta portion, g", perPorition: nil, per100g: food.delta ?? 0, type: .main))
+        
+        return data
+}
 }
 
 
 extension DetailsVC: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 + (minerals.count == 0 ? 0 : 1) + (vitamins.count == 0 ? 0 : 1)
+        return 2 + (minerals.count == 0 ? 0 : 1) + (vitamins.count == 0 ? 0 : 1)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
-            return macro.count
+            return userData.count
         case 1:
-            return minerals.count
+            return macro.count
         case 2:
+            return minerals.count
+        case 3:
             return vitamins.count
         default:
             return 0
@@ -61,6 +74,11 @@ extension DetailsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
         case 0:
+            if let cell = fullInfoTableView.dequeueReusableCell(withIdentifier: "foodDetailsCell", for: indexPath) as? FoodDetailsCell {
+                cell.initData(nutrient: userData[indexPath.row])
+                return cell
+            }
+        case 1:
             if macro[indexPath.row].type == .main {
                 if let cell = fullInfoTableView.dequeueReusableCell(withIdentifier: "foodDetailsCell", for: indexPath) as? FoodDetailsCell {
                     cell.initData(nutrient: macro[indexPath.row])
@@ -72,12 +90,12 @@ extension DetailsVC: UITableViewDelegate, UITableViewDataSource {
                     return cell
                 }
             }
-        case 1:
+        case 2:
             if let cell = fullInfoTableView.dequeueReusableCell(withIdentifier: "foodDetailsCell", for: indexPath) as? FoodDetailsCell {
                 cell.initData(nutrient: minerals[indexPath.row])
                 return cell
             }
-        case 2:
+        case 3:
             if let cell = fullInfoTableView.dequeueReusableCell(withIdentifier: "foodDetailsCell", for: indexPath) as? FoodDetailsCell {
                 cell.initData(nutrient: vitamins[indexPath.row])
                 return cell
@@ -91,10 +109,12 @@ extension DetailsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0:
-            return "Nutrition facts"
+            return "User info"
         case 1:
-            return "Minerals"
+            return "Nutrition facts"
         case 2:
+            return "Minerals"
+        case 3:
             return "Vitamins"
         default:
             return ""
