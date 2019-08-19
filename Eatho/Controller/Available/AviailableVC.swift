c6d7d5803207e96a84d275eef352d35a464add8a
serafima zykova <zykovas91@gmail.com>
2019-08-19 17:51:09 +0300

    extracted FoodVC

--- Eatho/Controller/Available/AviailableVC.swift
+++ Eatho/Controller/Available/AviailableVC.swift
@@ -8,25 +8,18 @@
 
 import UIKit
 
-class AviailableVC: UIViewController {
-
-    @IBOutlet weak var foodTable: UITableView!
-    @IBOutlet weak var searchBarHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
+class AviailableVC: FoodVC {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        foodTable.delegate = self
-        foodTable.dataSource = self
         
-        spinner.hidesWhenStopped = true
+        navigationItem.largeTitleDisplayMode = .never
         
         NotificationCenter.default.addObserver(self, selector: #selector(loadData), name: NOTIF_AUTH_DATA_CHANGED, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateData), name: NOTIF_FOOD_DATA_CHANGED, object: nil)
         
         configureRefreshControl()
-        registerForPreviewing(with: self, sourceView: foodTable)
+//        registerForPreviewing(with: self, sourceView: foodTable)
         loadData()
     }
     
@@ -36,13 +29,6 @@ class AviailableVC: UIViewController {
         if (AuthService.instance.isLoggedIn && DataService.instance.foods.count == 0) {
             loadData()
         }
-    }
-    
-    // Configure
-    
-    func configureRefreshControl() {
-        foodTable.refreshControl = UIRefreshControl()
-        foodTable.refreshControl?.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
     }
     
     
@@ -56,35 +42,18 @@ class AviailableVC: UIViewController {
             }
             
             DataService.instance.requestAvailableFoodItems(handler: { (success) in
+                super.foods = DataService.instance.foods
                 self.spinner.stopAnimating()
-                self.foodTable.reloadData()
+                self.foodTable!.reloadData()
             })
         } else {
             DataService.instance.clearData()
-            self.foodTable.reloadData()
+            self.foodTable!.reloadData()
         }
     }
     
     @objc private func updateData() {
         self.foodTable.reloadData()
-    }
-    
-    @objc func handleRefresh() {
-        DataService.instance.requestAvailableFoodItems(handler: { (success) in
-            self.foodTable.reloadData()
-            
-            // Dismiss the refresh control.
-            DispatchQueue.main.async {
-                self.foodTable.refreshControl?.endRefreshing()
-            }
-        })
-    }
-    
-    func openDetails(index: Int) {
-        guard let detailsVC = storyboard?.instantiateViewController(withIdentifier: "DetailsVC") as? DetailsVC else { return }
-        
-        present(detailsVC, animated: true, completion: nil)
-        detailsVC.initData(food: DataService.instance.foods[index])
     }
     
     func openUpdateVC(index: Int) {
@@ -93,42 +62,10 @@ class AviailableVC: UIViewController {
         present(editVC, animated: true, completion: nil)
         editVC.setupView(title: DataService.instance.foods[index].name!, food: DataService.instance.foods[index])
     }
-    
-    // Actions
-    
-    @IBAction func searchPressed(_ sender: Any) {
-        UIView.animate(withDuration: 0.3) {
-            if self.searchBarHeightConstraint.constant == 0 {
-                self.searchBarHeightConstraint.constant = 56
-            } else {
-                self.searchBarHeightConstraint.constant = 0
-            }
-            self.view.layoutIfNeeded()
-        }
-    }
 }
 
 
-extension AviailableVC: UITableViewDelegate, UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return DataService.instance.foods.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell") as? AvailableFoodCell {
-            if (indexPath.row < DataService.instance.foods.count) {
-                let food = DataService.instance.foods[indexPath.row]
-                cell.updateViews(foodItem: food)
-                return cell
-            }
-        }
-        return AvailableFoodCell()
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
+extension AviailableVC {
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let removeAction = UIContextualAction(style: UIContextualAction.Style.destructive, title: "Remove") { (acion: UIContextualAction, view: UIView, success: (Bool) -> Void) in
@@ -138,7 +75,7 @@ extension AviailableVC: UITableViewDelegate, UITableViewDataSource {
         removeAction.backgroundColor = EATHO_RED
         
         let revealDetailsAction = UIContextualAction(style: UIContextualAction.Style.normal, title: "Details") { (action: UIContextualAction, view: UIView, success: (Bool) -> Void) in
-            self.openDetails(index: indexPath.row)
+            super.openDetails(index: indexPath.row, usingUserData: true)
             success(true)
         }
         
@@ -152,20 +89,3 @@ extension AviailableVC: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-extension AviailableVC: UIViewControllerPreviewingDelegate {
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
-        guard let indexPath = foodTable.indexPathForRow(at: location) else { return nil }
-        guard let cell = foodTable.cellForRow(at: indexPath) else { return nil }
-        guard let detailsVC = storyboard?.instantiateViewController(withIdentifier: "DetailsVC") as? DetailsVC else { return nil }
-        
-        detailsVC.initData(food: DataService.instance.foods[indexPath.row])
-        previewingContext.sourceRect = cell.contentView.frame
-        
-        return detailsVC
-    }
-    
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
-        navigationController?.pushViewController(viewControllerToCommit, animated: true)
-//        show(viewControllerToCommit, sender: self)
-    }
-}
