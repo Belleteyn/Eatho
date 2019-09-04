a424d816a7d9150c3a220bd0a96735f2076578a9
serafima zykova <zykovas91@gmail.com>
2019-09-04 17:49:18 +0300

    modified EathoButton

--- Eatho/View/EathoButton.swift
+++ Eatho/View/EathoButton.swift
@@ -11,9 +11,21 @@ import UIKit
 @IBDesignable
 class EathoButton: UIButton {
 
-    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 4 {
+        didSet {
+            self.layer.borderWidth = borderWidth / 10
         }
     }
     
@@ -36,5 +48,7 @@ class EathoButton: UIButton {
 //        
 //        self.layer.insertSublayer(gradient, at: 0)
         self.layer.cornerRadius = cornerRadius
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth / 10
     }
 }
