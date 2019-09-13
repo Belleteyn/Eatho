b40ce725466d8f6f88e88399f19a4b0871068880
serafima zykova <zykovas91@gmail.com>
2019-09-13 17:33:39 +0300

    project refactoring

---
+++ Eatho/View/Cell/InputCell/SwitchSelectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchSelectionCell.swift
+//  Eatho
+//
+//  Created by Серафима Зыкова on 13/09/2019.
+//  Copyright © 2019 Серафима Зыкова. All rights reserved.
+//
+
+import UIKit
+
+class SwitchSelectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
