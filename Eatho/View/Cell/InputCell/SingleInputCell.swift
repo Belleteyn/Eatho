80f816752131a28b0a35d7d8e9a1072e3db1e43c
serafima zykova <zykovas91@gmail.com>
2019-09-20 15:49:14 +0300

    removed non-reusable stuff

--- Eatho/View/Cell/InputCell/SingleInputCell.swift
+++ Eatho/View/Cell/InputCell/SingleInputCell.swift
@@ -17,9 +17,6 @@ class SingleInputCell: UITableViewCell {
     @IBOutlet weak var leftLabel: UILabel!
     @IBOutlet weak var rightLabel: UILabel!
     
-    var inputType: NutritionInputType?
-    var userInputType: UserInfoInputType?
-    
     var inputChangedHandle: StringHandler?
     var inputFinishedHandle: StringHandler?
     var inputChangedDecimalHandler: NumberHandler?
@@ -33,7 +30,7 @@ class SingleInputCell: UITableViewCell {
     }
     
     /** also clears all optional values */
-    func setupView(title: String, additionalDesc desc: String, placeholder: String, text: String?) {
+    func setupView(title: String, additionalDesc desc: String, placeholder: String?, text: String?) {
         self.leftLabel.text = title
         self.rightLabel.text = desc
         self.textField.placeholder = placeholder
@@ -43,43 +40,6 @@ class SingleInputCell: UITableViewCell {
         self.inputChangedHandle = nil
         self.inpuFinishedDecimalHandler = nil
         self.inputChangedDecimalHandler = nil
-        self.inputType = nil
-        self.userInputType = nil
-    }
-    
-    func setupValues(inputType: NutritionInputType) {
-        self.inputType = inputType
-        switch inputType {
-        case .Calories:
-            leftLabel.text = "Calories"
-            rightLabel.text = "kcal"
-            textField.text = "\(truncateDoubleTail(SettingsService.instance.userInfo.nutrition.calories))"
-        default:
-            return
-        }
-    }
-    
-    func setupValues(inputType: UserInfoInputType) {
-        self.userInputType = inputType
-        
-        switch inputType {
-        case .Age:
-            textField.text = "\(SettingsService.instance.userInfo.age)"
-            leftLabel.text = "Age"
-            rightLabel.text = "years"
-        case .Height:
-            textField.text = "\(SettingsService.instance.userInfo.height)"
-            leftLabel.text = "Height"
-            rightLabel.text = "cm"
-        case .Weight:
-            textField.text = "\(SettingsService.instance.userInfo.weight)"
-            leftLabel.text = "Weight"
-            rightLabel.text = "kg"
-        case .CaloriesShortage:
-            textField.text = "\(SettingsService.instance.userInfo.caloriesShortage)"
-            leftLabel.text = "Calories shortage"
-            rightLabel.text = "kcal"
-        }
     }
 }
 
@@ -97,24 +57,6 @@ extension SingleInputCell: UITextFieldDelegate {
             inpuFinishedDecimalHandler(val)
             return
         }
-        
-        //TODO: remove !!!
-        
-        guard let type = userInputType else { return }
-        var info = SettingsService.instance.userInfo
-        
-        switch type {
-        case .Age:
-            info.age = Int(val)
-        case .Height:
-            info.height = val
-        case .Weight:
-            info.weight = val
-        case .CaloriesShortage:
-            info.caloriesShortage = val
-        }
-        
-        SettingsService.instance.userInfo = info
     }
 
     
@@ -129,19 +71,6 @@ extension SingleInputCell: UITextFieldDelegate {
         if let inputChangedDecimalHandler = inputChangedDecimalHandler {
             inputChangedDecimalHandler(val)
             return
-        }
-
-        guard let type = inputType else { return }
-        
-        var info = SettingsService.instance.userInfo
-        if type == .Calories {
-            info.nutrition.setCalories(kcal: val, updGrams: true)
-        }
-        
-        // update in storage and on server
-        SettingsService.instance.userInfo = info
-        if type == .Calories {
-            NotificationCenter.default.post(name: NOTIF_USER_NUTRITION_CHANGED, object: nil, userInfo: ["reloadIndices" : [1,2,3]])
         }
     }
 }
