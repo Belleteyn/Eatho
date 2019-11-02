72c85813505bb955d1c6906df401b78d0eff5d31
serafima zykova <zykovas91@gmail.com>
2019-11-02 15:17:07 +0300

    #23 changed buttons enabled/disabled states

--- Eatho/Controller/AuthControllers/RegisterNextVC.swift
+++ Eatho/Controller/AuthControllers/RegisterNextVC.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class RegisterNextVC: UIViewController, UITextFieldDelegate {
+class RegisterNextVC: UIViewController {
 
     @IBOutlet weak var inputTextField: UITextField!
-    @IBOutlet weak var separatorView: UIView!
-    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var registerButton: EathoButton!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     var email: String?
@@ -21,13 +20,17 @@ class RegisterNextVC: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        inputTextField.delegate = self
         inputTextField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("Password", comment: "Auth"), attributes: [NSAttributedString.Key.foregroundColor : LOGIN_PLACEHOLDER_COLOR])
+        inputTextField.addTarget(self, action: #selector(textFieldChangedHandle(_:)), for: .editingChanged)
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         view.addGestureRecognizer(tap)
         
         emailLabel.text = email
+    }
+    
+    @objc func textFieldChangedHandle(_ textField: UITextField) {
+        registerButton.isEnabled = (textField.text != "")
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
@@ -37,11 +40,7 @@ class RegisterNextVC: UIViewController, UITextFieldDelegate {
     
     @IBAction func registerPressed() {
         guard let email = email else { return }
-        guard let password = inputTextField.text, password != "" else {
-            errorLabel.text = ERROR_MSG_ALREADY_REGISTERED
-            separatorView.backgroundColor = EATHO_RED
-            return
-        }
+        guard let password = inputTextField.text, password != "" else { return }
         
         self.view.endEditing(false)
         spinner.startAnimating()
@@ -50,18 +49,12 @@ class RegisterNextVC: UIViewController, UITextFieldDelegate {
             self.spinner.stopAnimating()
             
             if let error = error { //todo: may be network error
-                self.errorLabel.text = ERROR_MSG_REGISTRATION_UNKNOWN_ERROR
-                self.separatorView.backgroundColor = EATHO_RED
+                self.showErrorAlert(title: "ERROR".localized, message: error.localizedDescription)
             } 
         }
     }
     
     @objc func handleTap() {
         view.endEditing(true)
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        self.errorLabel.text = ""
-        self.separatorView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.6588184932)
     }
 }
