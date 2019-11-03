20eb3475ae9fe538525d7f9346a987d50e816377
serafima zykova <zykovas91@gmail.com>
2019-11-03 14:22:26 +0300

    #10 added actions to last password reset view

--- Eatho/Controller/AuthControllers/PasswordRecoveryEnterPwdVC.swift
+++ Eatho/Controller/AuthControllers/PasswordRecoveryEnterPwdVC.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class PasswordRecoveryEnterPwdVC: BaseAuthVC {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    var email: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        infoLabel.text = ""
+        emailLabel.text = email
+        
+        textField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("Password", comment: "Auth"), attributes: [NSAttributedString.Key.foregroundColor : LOGIN_PLACEHOLDER_COLOR])
+        
+        textField.addTarget(self, action: #selector(textFieldChangeHandle(_:)), for: .editingChanged)
+        textField.becomeFirstResponder()
     }
     
+    @objc func textFieldChangeHandle(_ textField: UITextField) {
+        confirmButton.isEnabled = textField.text != nil && textField.text != ""
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        spinner.startAnimating()
+        
+        //todo request
+    }
 }
